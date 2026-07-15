b46e2d6c865b1bdc1c06b6e244bb03dd79c1d534
Bhupesh Srivastava <98334726+bhupeshmarine@users.noreply.github.com>
2026-07-15 00:52:09 -0500

    Refactor test.py to remove request and add prints
    Removed the request to the app and added print statements for response headers and text.

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
-import requests
-
-app_url = "PASTE_YOUR_APP_URL_HERE"
-
-response = requests.post(
-    f"{app_url}/invoke",
-    json={
-        "country": "US",
-        "prdm_naics_code": "541511",
-        "bbg_naics_code": "541511"
-    }
-)
-
 print("Status code:", response.status_code)
-print("Response:", response.json())
+print("Content-Type:", response.headers.get("content-type"))
+print("Response URL:", response.url)
+print("Response text:", response.text[:1000])
